eff90021dd81e260ac0299dad8a5656241811004
Chris Haub <71297455+chrishaub@users.noreply.github.com>
2020-12-02 21:56:36 -0600

    sql homework

---
+++ Submissions/QuickDBD-export.sql
@@ -0,0 +1,63 @@
+﻿
+CREATE TABLE Department (
+    dept_no varchar(10)   NOT NULL,
+    dept_name varchar(100)   NOT NULL,
+    CONSTRAINT pk_Department PRIMARY KEY (
+        dept_no
+     )
+);
+
+CREATE TABLE Titles (
+    title_id varchar(10)   NOT NULL,
+    title varchar(100)   NOT NULL,
+    CONSTRAINT pk_Titles PRIMARY KEY (
+        title_id
+     )
+);
+
+CREATE TABLE Employee (
+    emp_no int   NOT NULL,
+    emp_title_id varchar   NOT NULL,
+    birth_date date   NOT NULL,
+    first_name varchar(100)   NOT NULL,
+    last_name varchar(100)   NOT NULL,
+    sex varchar   NOT NULL,
+    hire_date date   NOT NULL,
+    CONSTRAINT pk_Employee PRIMARY KEY (
+        emp_no
+     )
+);
+
+CREATE TABLE Department_Employee (
+    emp_no int   NOT NULL,
+    dept_no varchar   NOT NULL
+);
+
+CREATE TABLE Manager (
+    dept_no varchar   NOT NULL,
+    emp_no int   NOT NULL
+);
+
+CREATE TABLE Salaries (
+    emp_no int   NOT NULL,
+    salary int   NOT NULL
+);
+
+ALTER TABLE Employee ADD CONSTRAINT fk_Employee_emp_title_id FOREIGN KEY(emp_title_id)
+REFERENCES Titles (title_id);
+
+ALTER TABLE Department_Employee ADD CONSTRAINT fk_Department_Employee_emp_no FOREIGN KEY(emp_no)
+REFERENCES Employee (emp_no);
+
+ALTER TABLE Department_Employee ADD CONSTRAINT fk_Department_Employee_dept_no FOREIGN KEY(dept_no)
+REFERENCES Department (dept_no);
+
+ALTER TABLE Manager ADD CONSTRAINT fk_Manager_dept_no FOREIGN KEY(dept_no)
+REFERENCES Department (dept_no);
+
+ALTER TABLE Manager ADD CONSTRAINT fk_Manager_emp_no FOREIGN KEY(emp_no)
+REFERENCES Employee (emp_no);
+
+ALTER TABLE Salaries ADD CONSTRAINT fk_Salaries_emp_no FOREIGN KEY(emp_no)
+REFERENCES Employee (emp_no);
+
